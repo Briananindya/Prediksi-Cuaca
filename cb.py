a623436448eb1ea08c86ec84cbac0682ce90a440
Brian Anindya <86071142+Briananindya@users.noreply.github.com>
2025-02-15 04:40:57 +0700

    biar ga error pakai st.experimental_rerun()
    mengatasi error

--- cb.py
+++ cb.py
@@ -368,7 +368,7 @@ if input_method == "Klik di Peta" and tmap and isinstance(tmap.get("last_clicked
         if "location" not in st.session_state or st.session_state.location != [lat, lon]:  
             st.session_state.location = [lat, lon]
             st.session_state.zoom_location = [lat, lon]
-            st.rerun()
+            sst.experimental_rerun()
 
 
 if input_method == "Pilih Provinsi":
